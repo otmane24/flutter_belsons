867bab5e41500a8953078e4c05d7843807a2abe0
otmane24 <benlaldj.otmane24l@gmail.com>
2025-01-17 09:07:32 +0100

    adaptive date picker

---
+++ lib/components/alert_dialog/date_picker_adaptive.dart
@@ -0,0 +1,94 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DatePickerAdaptive {
+  final BuildContext context;
+  final DateTime? initialDate;
+
+  DatePickerAdaptive(this.context, {this.initialDate});
+
+  Future<DateTime?> show() async {
+    if (Platform.isIOS) {
+      DateTime? selectedDate = initialDate ?? DateTime.now();
+      await showCupertinoModalPopup(
+        context: context,
+        builder: (BuildContext builder) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.r),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10.r,
+                  spreadRadius: 2.r,
+                )
+              ],
+            ),
+            height: 250.h,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 200.h,
+                      child: CupertinoDatePicker(
+                        initialDateTime: selectedDate,
+                        mode: CupertinoDatePickerMode.date,
+                        onDateTimeChanged: (DateTime newDate) {
+                          selectedDate = newDate;
+                        },
+                      ),
+                    ),
+                    const Divider(height: 1, thickness: 1, color: Colors.grey),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CupertinoButton(
+                          child: const Text(
+                            "Cancel",
+                            style: TextStyle(color: CupertinoColors.systemRed),
+                          ),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                        CupertinoButton(
+                          child: const Text(
+                            "Done",
+                            style: TextStyle(
+                              color: CupertinoColors.activeBlue,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {
+                            Navigator.of(context).pop(
+                              selectedDate,
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+
+      return selectedDate;
+    } else {
+      return await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2101),
+      );
+    }
+  }
+}
